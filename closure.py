ad6bd3139fcb861de1e4e8de4023ce8d5e5779dc
fred <major.freddy@yahoo.com>
2019-09-02 19:10:05 +0200

    a simple closure using a background thread

--- closure.py
+++ closure.py
@@ -2,6 +2,11 @@
 
 import requests
 from time import sleep
+import threading
+
+
+def print_t(str):
+    print(f'thread_id: {threading.get_ident()}: {str}')
 
 
 class Request:
@@ -9,35 +14,39 @@ class Request:
         self.closure = None
 
     def get(self, url):
-        print('I will dispatch the GET request now.')
-        res = requests.get(url)
-        # let's block the thread for some more time, to simulate a long-running
-        # request.
-        sleep(2)
-        print('Request finished. Calling the closure.')
-        if self.closure:
-            self.closure(res)
+        t = threading.Thread(target=self._get, args=(url, ))
+        t.start()
 
     def register_closure(self, closure_function):
         self.closure = closure_function
 
-    def _run(self):
-        pass
+    def _get(self, url):
+        print_t('I will dispatch the GET request now.')
+        res = requests.get(url)
+        # let's block the thread for some more time, to simulate a long-running
+        # request.
+        sleep(2)
+        print_t('Request has finished. Calling the closure.')
+        if self.closure:
+            self.closure(res)
 
 
 def main():
     def request_closure(request_results):
-        print('I am the closure. Received reply:')
-        print(request_results.text)
+        print_t(f'I am the closure. Received reply: {request_results.text}')
 
     request = Request()
     request.register_closure(request_closure)
     request.get('https://api.dopespot.io/v1/version')
 
-    # this is a dummy version. Everything happens in a single thread.
-    # the line below will be printed only after the `request.get` call finishes.
-    print('I am the main function. If you see this, it means the request '
-          'has already finished.')
+    # this version uses threads. While the background thread is working on the
+    # request, this one can do its stuff.
+    print_t('I am the main thread. I will do some work now.')
+    for i in range(10):
+        # do some complicated task...
+        print_t('work work work work work....')
+        sleep(0.5)
+    print_t('The main thread finished its work.')
 
 
 if __name__ == '__main__':
